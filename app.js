d58e43685b8c92b3356e42c27e47652f3f76d73e
Brenton McSweyn <brenton.mcsweyn@gmail.com.au>
2016-09-01 12:55:10 +1000

    Mongo and Auth

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const Koa = require('koa');
 const app = new Koa();
 const router = require('./routes');
 
+const jwt = require('koa-jwt');
+
 app.use(async function (ctx, next) {
   const start = new Date();
   await next();
@@ -22,6 +24,11 @@ app.use(async function (ctx, next) {
   await next();
 });
 
-app.use(router.routes())
-.use(router.allowedMethods())
-.listen(3000);
+const mongoose = require('mongoose');
+mongoose.connect(require('./config').database);
+mongoose.connection.on('error', () => {
+  console.info('Error: Could not connect to MongoDB. Did you forget to run `mongod`?'.red);
+});
+
+app.use(jwt({ secret: 'shared-secret' }).unless({ path: '/token' }));
+app.use(router.routes()).use(router.allowedMethods()).listen(3000);
--- routes.js
+++ routes.js
@@ -1,6 +1,12 @@
 let router = require('koa-router')();
 let invitations = require('./controllers/invitations');
 
+router.get('token', '/token', ctx => {
+  ctx.body = {
+    token: require('jsonwebtoken').sign({},  'shared-secret')
+  };
+});
+
 router.get('clients', '/clients', invitations.getAll);
 
 module.exports = router;
